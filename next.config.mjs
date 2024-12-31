1398370a3ff65203822372d9f2727012e43cbfab
Alexandre A <alexaardigo@gmail.com>
2024-12-31 19:22:38 +0000

    Merge remote-tracking branch 'origin/master'

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,6 @@ const nextConfig = {
     env: {
         TIMEZONEDB_API_KEY: process.env.TIMEZONEDB_API_KEY,
     },
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'pt-PT']
-    },
     images: {
         remotePatterns: [
             {
