7c60ff7c4b5cbb9ff5be6a9e03dd492cc15f8904
Alexandre A <alexaardigo@gmail.com>
2024-12-31 19:24:43 +0000

    Merge remote-tracking branch 'origin/master'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
 
 const zenKakuGothicNew = localFont({
   src: "./fonts/ZenKakuGothicNew-Regular.ttf",
@@ -28,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${zenKakuGothicNew.variable} antialiased`}
+        className={`${zenKakuGothicNew.variable} antialiased`}
       >
         {children}
       </body>
